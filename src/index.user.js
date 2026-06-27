c87fe22c81c4542146b032d94c401d90d3741f1a
Jiahao Luo <luoshitou9@gmail.com>
2026-06-27 21:51:36 +0800

    feat: add platform dispatcher and BaseAdapter

--- src/index.user.js
+++ src/index.user.js
@@ -14,5 +14,26 @@
 
 (function() {
     'use strict';
+
+    class BaseAdapter {
+        getConversations() { throw new Error('Not implemented'); }
+        injectCheckbox(node, callback) { throw new Error('Not implemented'); }
+        deleteConversation(node) { throw new Error('Not implemented'); }
+        getSidebarHeader() { throw new Error('Not implemented'); }
+    }
+
+    class OpenAIAdapter extends BaseAdapter {}
+    class GoogleAdapter extends BaseAdapter {}
+
+    const getAdapter = () => {
+        const host = window.location.hostname;
+        if (host.includes('chatgpt.com')) {
+            return new OpenAIAdapter();
+        } else if (host.includes('gemini.google.com')) {
+            return new GoogleAdapter();
+        }
+        return null;
+    };
+
     console.log('AI Chat Bulk Manager initialized');
 })();
